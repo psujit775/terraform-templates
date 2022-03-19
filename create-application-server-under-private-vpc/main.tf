b11b2efe64f56110f45c432804f5c15c57df79bc
sujit.patel <sujit.patel@oyerickshaw.com>
2022-03-20 03:17:39 +0530

    formatting files

--- create-application-server-under-private-vpc/main.tf
+++ create-application-server-under-private-vpc/main.tf
@@ -16,30 +16,30 @@ provider "aws" {
 
 #Creating security group
 resource "aws_security_group" "demo-sg" {
-  name = "${var.environment}-ec2-sg"
+  name        = "${var.environment}-ec2-sg"
   description = "security group for ${var.environment}-ec2-instance"
-  vpc_id = var.vpcid
+  vpc_id      = var.vpcid
 
   #To Allow SSH
   ingress {
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
   #to allow tomcat
   ingress {
-    from_port = 8080
-    protocol = "tcp"
-    to_port = 8080
+    from_port   = 8080
+    protocol    = "tcp"
+    to_port     = 8080
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 }
@@ -47,21 +47,21 @@ resource "aws_security_group" "demo-sg" {
 
 #Creating EC2 instance in private subnet with application server.
 resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  user_data     = file("init-script.sh")
-  subnet_id = var.private_subnet_id
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  user_data                   = file("init-script.sh")
+  subnet_id                   = var.private_subnet_id
   associate_public_ip_address = var.enable_public_ip
-  key_name = var.keyname
+  key_name                    = var.keyname
 
   vpc_security_group_ids = [
     aws_security_group.demo-sg.id
   ]
-  
-  depends_on = [ aws_security_group.demo-sg ]
+
+  depends_on = [aws_security_group.demo-sg]
 
   tags = {
-    Name = "${var.environment}-instance"
+    Name        = "${var.environment}-instance"
     Environment = var.environment
   }
 }
--- create-application-server-under-private-vpc/vars.tf
+++ create-application-server-under-private-vpc/vars.tf
@@ -8,31 +8,31 @@ variable "environment" {
 }
 
 variable "ami" {
-  default = "ami-0851b76e8b1bce90b"
+  default     = "ami-0851b76e8b1bce90b"
   description = "ubuntu 20.04 LTS image"
 }
 
 variable "instance_type" {
-  default = "t3a.micro"
+  default     = "t3a.micro"
   description = "instance type of ec2."
 }
 
 variable "vpcid" {
-  default = "Replcae with your VPC ID"
-  description = "VPC id of an existing vpc" 
+  default     = "Replcae with your VPC ID"
+  description = "VPC id of an existing vpc"
 }
 
 variable "private_subnet_id" {
-  default = "Replcae with your private subnet ID"
+  default     = "Replcae with your private subnet ID"
   description = "Existing private subnet id"
 }
 
 variable "enable_public_ip" {
-  default = "false"
+  default     = "false"
   description = "Enable/Disable public ip."
 }
 
-variable "keyname" { 
-  default = "Replace with your key pair name"
-  description = "key pair name" 
+variable "keyname" {
+  default     = "Replace with your key pair name"
+  description = "key pair name"
 }
